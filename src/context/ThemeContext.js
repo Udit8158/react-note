77d4576ab1716ed86ecad5570c6d317debf93627
Udit Kundu <kunduudit8158@gmail.com>
2022-12-20 14:32:57 +0530

    add just theme mode with local storage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,11 +6,21 @@ export const ThemeContext = createContext({
 });
 
 export const ThemeContextProvider = ({ children }) => {
-  const [themeMode, setThemeMode] = useState("light");
+  const localStorageTheme = localStorage.getItem("react-note-app-theme");
+
+  const [themeMode, setThemeMode] = useState(
+    localStorageTheme ? localStorageTheme : "light"
+  );
 
   const toggleModeHandler = (themeMode) => {
-    if (themeMode === "light") setThemeMode("dark");
-    if (themeMode === "dark") setThemeMode("light");
+    if (themeMode === "light") {
+      setThemeMode("dark");
+      localStorage.setItem("react-note-app-theme", "dark");
+    }
+    if (themeMode === "dark") {
+      setThemeMode("light");
+      localStorage.setItem("react-note-app-theme", "light");
+    }
   };
 
   const initialCtx = {
